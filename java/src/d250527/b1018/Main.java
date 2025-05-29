c56c2b8538a22780ab27c4b127b98a9415e20e3b
SJ <ligerssj@naver.com>
2025-05-30 08:32:27 +0900

    깃허브 저장소 변경

---
+++ java/src/d250527/b1018/Main.java
@@ -0,0 +1,53 @@
+package d250527.b1018;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 체스판
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String buff = br.readLine();
+
+        int n = Integer.parseInt(buff.split(" ")[0]);
+        int m = Integer.parseInt(buff.split(" ")[1]);
+
+        // 값을 받아서 배열로 저장
+        char[][] arr = new char[n][];
+        for(int i=0; i<n; i++){
+            String tmp = br.readLine();
+            arr[i] = tmp.toCharArray();
+        }
+
+        // 최종 값을 담을 변수
+        int count = Integer.MAX_VALUE;
+        // 비교를 위한 배열
+        char[] checker = {'W','B'};
+        // 전체 범위
+        for(int i = 0; i < n - 8 + 1; i++){
+            for(int j = 0; j < m - 8 + 1; j++){
+                
+                int wCount = 0, bCount = 0;
+                // 잘라낸 범위
+                for(int k = i; k < i + 8; k++){
+                    for(int l = j; l < j + 8; l++){
+                        // 시작이 W일때 B일때를 나누어서 카운트
+                        int idx = (i + j) % 2;
+                        if((k + l) % 2 == 0) {
+                            if(arr[k][l] != checker[idx]) wCount++;
+                            else bCount++;
+                        } else {
+                            if(arr[k][l] == checker[idx]) wCount++;
+                            else bCount++;
+                        }
+                    }
+                }
+                // 더 작은 값을 반영
+                int subCount =  wCount < bCount ? wCount : bCount;
+                count = count < subCount ? count : subCount;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
---
+++ java/src/d250527/b2798/Main.java
@@ -0,0 +1,43 @@
+package d250527.b2798;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 블랙잭
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] buff = br.readLine().split(" ");
+        int n = Integer.parseInt(buff[0]);
+        int m = Integer.parseInt(buff[1]);
+
+        // 값을 배열에 저장하고 정렬
+        buff = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(buff[i]);
+        Arrays.sort(arr);
+
+        // 가장 큰 값(i)부터 하나씩 올리면서 내부에서 전체적으로 반복
+        int max = 0;
+        iLoop: for(int i = 2; i < n; i++){
+            jLoop: for(int j = 1; j < i; j++){
+                for(int k = 0; k < j; k++){
+                    int curr = arr[i] + arr[j] + arr[k];
+                    // 목표 값을 넘어버리면 가장 안쪽 루프 스킵
+                    if(curr > m) continue jLoop;
+                    else {
+                        if(curr > max) {
+                            max = curr;
+                            // 목표 값에 도달하면 루프 종료
+                            if(max == m) break iLoop;
+                        }
+                    } 
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
