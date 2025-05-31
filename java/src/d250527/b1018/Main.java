fe033864e2cfad79220fe02125bea4ae150197ba
passing7by <jinam0717@gmail.com>
2025-05-31 19:57:11 +0900

    b1018 완료

--- java/src/d250527/b1018/Main.java
+++ java/src/d250527/b1018/Main.java
@@ -3,33 +3,27 @@ package d250527.b1018;
 import java.util.Scanner;
 
 /*
- * 어떻게 접근해야 하지......
- * 8*8로 자르는 모든 경우의 수를 생각해 봤을 때
- * B/W 중 그 수가 적은 색깔을 칠하고, 칠한 갯수 저장
- * 칠한 갯수가 가장 적은 것 출력
- * 
- * 8*8로 자르는 모든 경우의 수....를 어떻게 다룰까
- * 8*8의 2차원 배열을 생성하고, 거기다 매번 초기화를 해줄까
- * 
- * M*N의 보드.... 입력은 N M의 순서로 들어옴
- * 
- * *****
+ * 체스판을 칠하는 경우의 수는 두 가지
+ * 1. 흰검흰검...
+ * 2. 검흰검흰...
  * 
  */
-public class Main {
+public class Main2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int iNum = sc.nextInt();
-		int jNum = sc.nextInt();
+		// N, M을 입력받음
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 		sc.nextLine();
 		
-		String[][] board = new String[iNum][jNum];		
+		// N*M만큼 입력받은 값들을 저잘할 배열 생성
+		char[][] board = new char[n][m];		
 		
-		for (int i = 0; i < board.length; i++) { // 10번 반복
+		for (int i = 0; i < board.length; i++) { // N번 반복
 			String inputLine = sc.nextLine();
-			for (int j = 0; j < board[i].length; j++) { // 13번 반복
-				board[i][j] = inputLine.charAt(j)+"";
+			for (int j = 0; j < board[i].length; j++) { // M번 반복
+				board[i][j] = inputLine.charAt(j);
 			}
 		}
 		
@@ -43,44 +37,23 @@ public class Main {
 		int endIIdx = 7;
 		int startJIdx = 0;
 		int endJIdx = 7;
-//		int countI = 1;
-//		int countJ = 1;
 		
-		int bNum = 0;
-		int wNum = 0;
-		
-		int minColoringNum = Integer.MAX_VALUE;
-		
+		// TODO 어떻게 빠져나올지 생각.....
 		while (true) {	
-			for (int i = startIIdx; i < endIIdx; i++) {
-				for (int j = startJIdx; j < endJIdx; j++) {
-					switch (board[i][j]) {
-						case "W" -> wNum++;
-						case "B" -> bNum++;
-					}
+			for (int i = startIIdx; i <= endIIdx; i++) { // 반복문I(열)
+				for (int j = startJIdx; j <= endJIdx; j++) { // 반복문J(행)
+					System.out.println("i: "+i+" j: "+j);
+					
 				}
 				
-				if(endJIdx == board[i].length - 1) break;
-				
-				startJIdx++;
-				endJIdx++;
+				System.out.println();
+
+				startIIdx++;
+				endIIdx++;
 			}
 			
-			int coloringNum = Math.abs(wNum-bNum);
-			
-			if (coloringNum == 0) {
-				System.out.println(0);
-				return;
-			} else if (coloringNum < minColoringNum) {
-				minColoringNum = coloringNum;
-			}
-			
-			if(endIIdx == board.length - 1) break;
-			
-			startIIdx++;
-			endIIdx++;
+			startJIdx++;
+			endJIdx++;
 		}
-		
-		System.out.println(minColoringNum);
 	}
 }
