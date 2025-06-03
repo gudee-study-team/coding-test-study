f195b03bdeaca82658ceaf76ec3f827cd9f5704c
SJ <ligerssj@naver.com>
2025-06-04 08:48:50 +0900

    import 수정

--- java/src/d250527/b1018/Main.java
+++ java/src/d250527/b1018/Main.java
@@ -1,7 +1,6 @@
 package d250527.b1018;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
