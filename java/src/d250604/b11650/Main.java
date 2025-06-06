03124309f3b0297183844f6132abdb01e0b763b7
SJ <ligerssj@naver.com>
2025-06-06 19:10:56 +0900

    11650 Pair 클래스 분리

--- java/src/d250604/b11650/Main.java
+++ java/src/d250604/b11650/Main.java
@@ -30,14 +30,14 @@ public class Main {
 
         System.out.println(sb);
     }
+}
 
-    static class Pair {
-        int first;
-        int second;
+class Pair {
+    int first;
+    int second;
 
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
     }
 }
