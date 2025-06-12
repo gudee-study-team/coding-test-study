65b66bf4e1f477807776be23630f70eab5414cd7
jeong gil hun <wjdrlfgns1@naver.com>
2025-06-12 17:03:37 +0900

    b9012

--- java/src/d250610/b2164/Main.java
+++ java/src/d250610/b2164/Main.java
@@ -4,24 +4,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-  //n이 2의 n제곱이면 n 출력
-  // n이 2의n제곱인 숫자로부터 떨어진 값만큼 2의 배수 출력
+  // n이 2의 n제곱이면 n 출력
+
+  // n이 2의거듭제곱인 숫자로부터 떨어진 값만큼 2의 배수 출력
   // 예) 17인경우 16으로부터 1만큼 떨어져있으므로 2가 출력됨
-  // 
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int num = Integer.parseInt(br.readLine());
     int power_2 = 1;
-    while (power_2<num) { // num에서 가장가까운 2의 거듭제곱 수를 찾음
+    while (power_2<num) { // num보다 큰 가장가까운 2의 거듭제곱 수를 찾음
       power_2 *=2;
     }
-    if(power_2!=1) power_2/=2; // 1이 아니라면 2로 한번 나눠줘서 num보다 작은 가장 가까운 2의거듭제곱수를 만듬
-    num=num- power_2; // num과 2의거듭제곱 과의 차를 구함
-    if(num==0){// 차가 0이라면 2의 거듭제곱수 이므로 그값 그대로 출력
-      System.out.println(power_2);
+    if(power_2!=1) power_2/=2; // 1이 아니라면 2로 한번 나눠줘서 num보다 작은 가장 가까운 2의거듭제곱수를 만듬 
+    int sub;
+    sub=num- power_2; // num과 2의거듭제곱 과의 차를 구함
+    if(sub==0){// 차가 0이라면 2의 거듭제곱수 이므로 그값 그대로 출력
+      System.out.println(num);
     }
     else{ // 아닌경우 2를 곱해줘서 출력
-      System.out.println(num*2);
+      System.out.println(sub*2);
     }
   } 
 }
--- java/src/d250610/b9012/Main.java
+++ java/src/d250610/b9012/Main.java
@@ -1,5 +1,37 @@
 package d250610.b9012;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-  
+  public static void main(String[] args) throws Exception {
+    
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  int num = Integer.parseInt(br.readLine()); // 첫번째 숫자 입력 받음
+  int count; // '(' 의 개수
+  String str[];
+  StringBuilder sb = new StringBuilder();
+
+  for(int i =0; i< num; i++){ 
+    //문자열을 입력 받아서 str 배열에 하나씩 저장
+    count =0;
+    str= br.readLine().split("");
+    for(int j = 0 ; j<str.length; j++){
+      //str배열에서 하나씩 꺼내와서 순서대로 비교
+    if(str[j].equals("(")){count++; continue;}
+    // ( 이들어오면 count를 1증가시킴
+
+    if(count>0) count --;
+    // count>0일때 즉 '(' 이 있을 때 ')'이 들어오면 count를 1 감소 시킴
+    else {count = 1; break;}
+    // count>0이 아닐때 즉 '(' 이 없을 때 ')'가 들어오면 VPS가 성립되지 않으므로 바로 탈출
+
+    }
+  if(count!=0) // count 가 0이 아니라면 VPS가 아니기 때문에 NO를 출력
+  sb.append("NO").append("\n");
+  else   sb.append("YES").append("\n"); // count가 0이면 VPS가 되기에 YES를 출력
+  }
+  System.out.println(sb);
+
+  }
 }
