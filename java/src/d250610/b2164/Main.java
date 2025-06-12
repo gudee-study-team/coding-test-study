39fb6372ddc5cc81d131a0bcf03f050f4f1efc1a
passing7by <passing7by.naver.com>
2025-06-12 18:48:46 +0900

    b2164 푸는 중

--- java/src/d250610/b2164/Main.java
+++ java/src/d250610/b2164/Main.java
@@ -35,31 +35,29 @@ public class Main {
 //		long end = System.nanoTime();
 //		System.out.println(end-start);
 		
-		// StringBuilder 사용
-		long start = System.nanoTime();
-		// 카드 갯수 ~ 1 까지 한 장씩 위로 쌓는다.
-		StringBuilder sb = new StringBuilder();
-		for (int i = cardNum; i >= 1; i--) {
-			sb.append(i);
+		if (cardNum == 1) {
+			System.out.println(cardNum);
+			return;
 		}
 		
-		while (true) {
-			// 맨 위의 카드를 버린다.
-//			System.out.println(sb.deleteCharAt(sb.length()-1));	
-			sb.deleteCharAt(sb.length()-1);
-			System.err.println(sb);
-//			System.out.println(sb);
-			// 맨 위의 카드를 맨 아래로 옮긴다.
-			char topCard = sb.charAt(sb.length()-1);
-			sb = String.valueOf(topCard) + sb.deleteCharAt(sb.length()-1);
-			System.out.println(sb);
-			// 카드가 한 장 남으면 반복을 멈추고 카드 번호를 출력한다.
-			if (sb.length() == 1) {
-				System.out.print(sb);
-				break;
+		
+		// 2로 나눈 나머지가 0이면서 몫이 짝수인 수 중 가장 큰 수가 남음
+		for (int i = cardNum; i >= 1; i--) {
+			// 2의 제곱수인지 검증
+			// 어떻게?
+			// 2로 나눈 몫
+			// 그 몫을 가지고 또 2로 나누고
+			// 몫이 7 이하가 됐을 때, 2/4/8이면 2의 제곱수
+			int target = i;
+			while (target > 7) {
+				target /= 2;
+			}
+			
+			
+			if (target == 2 || target == 4 || target == 8) {
+				System.out.println(i);
+				return;
 			}
 		}
-		long end = System.nanoTime();
-//		System.out.println(end-start);
 	}
 }
