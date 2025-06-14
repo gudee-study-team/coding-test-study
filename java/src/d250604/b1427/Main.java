ea71028f2168399a6b780fa6a4ad899637d0efbc
passing7by <jinam0717@gmail.com>
2025-06-14 20:19:13 +0900

    노트북에서의 수정 사항

--- java/src/d250604/b1427/Main.java
+++ java/src/d250604/b1427/Main.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-/*
- * 어떻게 한담........
- * 쉽게 간다고 치면
- * n을 문자열로 받아서 char 배열로 만든 다음에 그걸 내림차순으로 Arrays.sort 하면 되긴 하는데...
- */
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -16,6 +11,7 @@ public class Main {
 //		String n = sc.nextLine();
 		String n = sc.nextLine();
 		
+		// List 사용
 //		long start = System.nanoTime();
 //		List<Character> list = new ArrayList<Character>();
 //		for (int i = 0; i < n.length(); i++) {
