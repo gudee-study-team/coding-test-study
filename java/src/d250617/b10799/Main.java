92a4fa3cbc31dbe56c3aa68a5636c880080374e0
jeong gil hun <wjdrlfgns1@naver.com>
2025-06-19 17:51:28 +0900

    17478 푸는중

---
+++ java/src/d250617/b10799/Main.java
@@ -0,0 +1,12 @@
+package d250617.b10799;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) {
+    // '()'이 들어왔을 때 닫히지 않은 '('의 개수가 레이저가 자르는 쇠막대기의 개수가 된다.
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+  }
+}
