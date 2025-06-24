4d4599b9efa9ca7b85738da0a2e3df0820136856
wjdrlfgns1 <wjdrlfgns1@naver.com>
2025-06-24 11:54:26 +0900

    수정

--- java/src/d250617/b10799/Main.java
+++ java/src/d250617/b10799/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws Exception {
