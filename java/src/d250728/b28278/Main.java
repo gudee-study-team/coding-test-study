743179316d7e30f90300851440e5183605d6b4b6
passing7by <jinam0717@gmail.com>
2025-08-04 14:54:53 +0900

    b28278 푸는 중

---
+++ java/src/d250728/b28278/Main.java
@@ -0,0 +1,20 @@
+package d250728.b28278;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * 1: d.add(x)
+ * 2: d.pop() or -1
+ * 3: d.size()
+ * 4: d.isEmpty()? 1 : 0
+ * 5: d.getLast() or -1
+ */
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		Deque<Integer> d = new ArrayDeque<Integer>();
+		
+	}
+}
