86d8adf801dfb93f7f99907bc7968e68173a3430
Coma <comasocean@gmail.com>
2025-08-11 17:42:45 +0900

    WIP: MyArrayList

---
+++ java/src/d250805/arraylist/MyArrayList.java
@@ -0,0 +1,108 @@
+package d250805.arraylist;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E> {
+  private Object[] array;
+  private int pointer;
+
+  private static final int DEFAULT_CAPACITY = 10;
+
+  public MyArrayList() {
+    this(DEFAULT_CAPACITY);
+  }
+
+  public MyArrayList(int size) {
+    if (size < 0) throw new IllegalArgumentException();
+    if (size < 10) size = DEFAULT_CAPACITY;
+
+    this.array = new Object[size];
+    this.pointer = 0;
+  }
+
+  private void resize() {
+    int arrLength = this.array.length;
+
+    if (this.pointer >= arrLength) {
+      this.array = Arrays.copyOf(this.array, arrLength * 2);
+    }
+
+    if (this.pointer < (arrLength / 3)) {
+      this.array = Arrays.copyOf(this.array, arrLength / 2);
+    }
+  }
+
+  @Override // 요소를 추가
+  public boolean add(E value) {
+    resize();
+
+    this.array[pointer] = value;
+    pointer++;
+
+    return true;
+  }
+
+  @Override // 요소를 특정 위치에 추가
+  public void add(int index, E value) {
+    if (index > pointer || index < 0) throw new ArrayIndexOutOfBoundsException();
+
+    resize();
+
+    for (int i = pointer - 1; i >= index; i--) {
+      this.array[i + 1] = this.array[i];
+    }
+
+    this.array[index] = value;
+    pointer++;
+  }
+
+  @Override // 요소를 삭제
+  public boolean remove(Object value) {
+    return false;
+  }
+
+  @Override // 특정 위치에 있는 요소를 삭제
+  public E remove(int index) {
+    return null;
+  }
+
+  @Override // 요소 가져오기
+  public E get(int index) {
+    return null;
+  }
+
+  @Override // 특정 위치에 있는 요소를 새 요소로 대체
+  public void set(int index, E value) {
+
+  }
+
+  @Override // 특정 요소가 리스트에 있는지 여부를 확인
+  public boolean contains(Object value) {
+    return false;
+  }
+
+  @Override // 특정 요소가 몇 번째 위치에 있는지를 반환 (순차 검색)
+  public int indexOf(Object value) {
+    return 0;
+  }
+
+  @Override // 특정 요소가 몇 번째 위치에 있는지를 반환 (역순 검색)
+  public int lastIndexOf(Object o) {
+    return 0;
+  }
+
+  @Override // 요소의 개수를 반환
+  public int size() {
+    return 0;
+  }
+
+  @Override // 요소가 비어있는지
+  public boolean isEmpty() {
+    return false;
+  }
+
+  @Override // 요소를 모두 삭제
+  public void clear() {
+
+  }
+}
--- java/src/d250805/b11660/Main.java
+++ java/src/d250805/b11660/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class Main {
   private static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
