00692fe15653276aa01fcf535567c1fb872c4c95
passing7by <jinam0717@gmail.com>
2025-08-12 20:56:12 +0900

    b11660 푸는 중

--- java/src/d250804/b11660/Main.java
+++ java/src/d250804/b11660/Main.java
@@ -13,14 +13,9 @@ import java.lang.*;
  * n: 표의 크기(1~1,024) - n*n
  * m: 합을 구해야하는 횟수(1~100,000)
  * 
- * 1 혹은 2가 0이라면
- * 1이 0이라면 왼쪽 줄 뺄 필요 없음
- * 2가 0이라면 위쪽 줄 뺄 필요 없음
- * 
  * x축 줄들의 합 구해놓고
  * 전체합 구해놓고
- * y축 줄들의 합 구해놓고
- * 전체합 - [x축 합] - [y축 합] + [x축과 y축이 겹치는 부분의 숫자들의 합]
+ * 전체합 - [x축 합] - ?
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -35,35 +30,32 @@ public class Main {
 		int num[][] = new int[n][n];
 		int sum = 0;
 		int sumX[] = new int[n];
-		int sumY[] = new int[n];
 		int idx = 0;
 		for(int i = 0; i < n; i++) {
 			String line[] = br.readLine().split(" ");
 			
 			int sumRow = 0;
-			int sumcol = 0;
 			for (int j = 0; j < line.length; j++) {
-				
 				
 				num[i][j] = Integer.parseInt(line[j]);
 				sum += num[i][j];
 				sumRow += num[i][j];
-				sumcol += num[j][i]; // 너무 헷갈림.....
 			}
 			sumX[i] = sumRow;
-			sumY[i] = sumcol;
 		}
 		
 		for (int i : sumX) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
-		for (int i : sumY) {
-			System.out.print(i + " ");
-		}
-		
 
-		
+		for (int i = 0; i < m; i++) {
+			String Line[] = br.readLine().split(" ");
+			int lineNum[] = new int[4];
+			// x값: 1, 3
+			// y값: 2, 4
+//			sumX[lineNum[3]] - sumX[lineNum[3] - 1];
+		}
 		
 	}
 }
